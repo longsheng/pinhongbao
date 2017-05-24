a3fefd7744281e1055fcc27c1bff890c478220fb
longsheng@yeah.net <longsheng>
2017-05-24 17:32:17 +0800

    ?

--- myfunc.js
+++ myfunc.js
@@ -1,11 +1,17 @@
 console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
 define(function(require){
-	return{
-		hello1 : function() {
-			console.log("hello1");
-		},
-		hello2 : function() {
-			console.log("hello2");
+	var justep = require("$UI/system/lib/justep");
+	var Media = require("cordova!cordova-plugin-media");
+	var myfunc = {};
+	myfunc.playmusic = function(filename) {
+		if (justep.Browser.isX5App) {
+			var src = window.location.origin + require.toUrl(filename);
+			this.mediaRec = new Media(src);
+			this.mediaRec.play();
+		} else {
+			console.log("无法播放音乐...");
 		}
 	};
+	window.myfunc = myfunc; //js表达式需要使用
+	return myfunc;
 });
